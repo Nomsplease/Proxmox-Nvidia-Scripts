f2211d44548f033b54d945893a4eb409aaa04d6a
Noms <Nomsplease.code@gmail.com>
2022-06-09 15:30:25 -0400

    Update CGroup helper for safety
    Added validation that LXC is not running when we attempt to write its config file

--- LXC Scripts/LXC-CGroup-Helper.sh
+++ LXC Scripts/LXC-CGroup-Helper.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 read -p 'Input VMID of LXC to modify: ' LXCID
+
+if [ ! -f /etc/pve/lxc/$LXCID.conf ]; then
+    echo "LXC with ID $LXCID does not exist. Is this ID correct?"
+    echo "This script must be run on the local host of the container."
+fi
+
 UNSORTED_CGROUPS=("$@")
 SORTED_CGROUPS=("$@")
+
 for item in $(find /dev/ -maxdepth 1 -not -type d -name nvidia*)
 do
     UNSORTED_CGROUPS+=(`ls -alh $item | awk '{print $5}' | sed 's/,//'`)
 done
+
 SORTED_CGROUPS=($(printf "%s\n" "${UNSORTED_CGROUPS[@]}" | sort -u))
 echo "CGroups found: ${SORTED_CGROUPS[@]}"
 
@@ -33,9 +41,25 @@ function Check_Update_Mount_Entries {
     fi
 }
 
+LXC_STATUS=`pct status $LXCID | sed "s/status: //"`
+if [ $LXC_STATUS == "running" ]; then
+    while true; do
+        echo "Container is currently running."
+        read -p "Would you like us to issue a shutdown to it? " yn
+        case $yn in
+            [Yy]* ) pct shutdown $LXCID; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
 for cgroup in "${SORTED_CGROUPS[@]}"
 do
     Check_Update_CGroup $cgroup
 done
 
 Check_Update_Mount_Entries
+
+echo "All entries have been added to /etc/pve/lxc/$LXCID.conf"
+echo "LXC was not started automatically, please start container with 'pct start $LXCID' or via the UI"
